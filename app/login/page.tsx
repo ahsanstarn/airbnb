beeec99c25e14655c3fa29ab47729b477b979d8d
ahsanstarn <ahsanstarn@gmail.com>
2026-05-10 16:01:35 +0500

    Fix unescaped apostrophe in login page to resolve Vercel linting failure

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -102,7 +102,7 @@ export default function LoginPage() {
           </form>
 
           <div className={styles.footer}>
-            <p>Don't have an account? <Link href="/login">Sign up</Link></p>
+            <p>Don&apos;t have an account? <Link href="/login">Sign up</Link></p>
           </div>
         </div>
       </div>
